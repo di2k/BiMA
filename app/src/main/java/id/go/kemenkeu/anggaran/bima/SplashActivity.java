d0d27733bc012c3b48e1629025e04af2c4b01018
Didik Setiawan <didik.s@kemenkeu.go.id>
2019-09-12 14:03:04 +0700

    Add splash screen

---
+++ app/src/main/java/id/go/kemenkeu/anggaran/bima/SplashActivity.java
@@ -0,0 +1,28 @@
+package id.go.kemenkeu.anggaran.bima;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private int timer = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, timer);
+
+    }
+}
