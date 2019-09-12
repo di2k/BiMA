62626e8135a99486283fa0838e53595532c5b4d5
didik setiawan <di2k.setiawan@gmail.com>
2019-09-12 09:24:33 +0700

    BiMA
    First ...

---
+++ app/src/main/java/id/go/kemenkeu/anggaran/bima/MainActivity.java
@@ -0,0 +1,14 @@
+package id.go.kemenkeu.anggaran.bima;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
